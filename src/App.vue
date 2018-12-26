c2e99ceb40f61d870878a4e60c56d6f2afaf8216
junfeidao <823201662@qq.com>
2018-12-26 16:06:20 +0800

    feat(components/lzr.vue): 添加李兆瑞的路由

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/lzr">李兆瑞</router-link>
+      <!-- <router-link to="/about">About</router-link> -->
+      <!-- <router-link to="/qsf">qsf</router-link> -->
     </div>
     <router-view/>
   </div>
---
+++ src/components/lzr.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="lzr">
+        <h1>李兆瑞的项目</h1>
+    </div>
+</template>
