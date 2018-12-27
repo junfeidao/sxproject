7f50b87888718d1494a395f454bc5cc503cc238f
junfeidao <823201662@qq.com>
2018-12-27 11:13:00 +0800

    fix(components): 修改六个路由的路径

--- src/components/thq.vue
+++ src/components/thq.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="thq">
         <a href=""></a><a href=""></a>
-        <a href="https://github.com/junfeidao/sxproject/tree/develop/src/pages/Tao%20haoqin" target="_blank" rel="noopener noreferrer"><h2>陶毫钦的项目</h2></a>
+        <a href="https://github.com/junfeidao/sxproject/tree/develop/src/pages/thq" target="_blank" rel="noopener noreferrer"><h2>陶毫钦的项目</h2></a>
         <a href=""></a><a href=""></a><a href=""></a>
     </div> 
 </template>
--- src/components/wzh.vue
+++ src/components/wzh.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="wzh">
         <a href=""></a><a href=""></a><a href=""></a>
-        <a href="https://github.com/junfeidao/sxproject/tree/develop/src/pages/Wang%20zihang" target="_blank" rel="noopener noreferrer"><h2>王梓航的项目</h2></a>
+        <a href="https://github.com/junfeidao/sxproject/tree/develop/src/pages/wzh" target="_blank" rel="noopener noreferrer"><h2>王梓航的项目</h2></a>
         <a href=""></a><a href=""></a>
     </div> 
 </template>
