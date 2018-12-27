9fc5ec64b293218d77878504b2a7946062f271a4
lzr950228 <465236585@qq.com>
2018-12-27 18:52:19 +0800

    docs(scr): src

---
+++ src/pages/lzr/src/lzr.vue
@@ -0,0 +1,36 @@
+<template>
+    <div style="width: 600px;height:400px;"></div>
+</template>
+<script>
+let echarts=require('echarts');
+export default {
+  data(){
+
+  },
+  methods:{
+      drawLine(){
+      this.myChart.setOption({
+
+        xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [{
+        data: [3, 2, 5, 4, 5, 2, 3],
+        type: 'line',
+        areaStyle: {}
+    }]
+
+})
+      }
+  },
+  mounted(){
+     this.myChart=echarts.init(this.$el);//初始化
+     this.drawLine()
+  }
+}
+</script>
