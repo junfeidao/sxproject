14be414b64f06d796867b0a4ac2c412273b96f16
taohaoqin <445943123@qq.com>
2018-12-29 09:04:46 +0800

    feat: 把components/,Table.vue引入到pags/,thq.vue里

--- src/components/cp-thq-table/src/Table.vue
+++ src/components/cp-thq-table/src/Table.vue
@@ -1,46 +1,49 @@
 <template>
-    <el-table :data="tableData" style="width: 100%">
-     <el-table-column  type="index" label="序号" width="50"></el-table-column>
-     <el-table-column prop="name" label="种类" width="90"></el-table-column>
-     <el-table-column prop="number" label="销量" width="70"></el-table-column> 
-    </el-table>
-</template>
 
-<script>
-    export default {
-     data() {
-            return {
-                tableData: [
-                    {
-                        name: '衬衫',
-                        number: '5'
-                    }, 
-                    {
-                        name: '羊毛衫',
-                        number: '20'
-                    }, 
-                    {
-                        name: '雪纺衫',
-                        number: '36'
-                    }, 
-                    {
-                        name: '裤子',
-                        number: '10'
-                    },
-                    {
-                        name: '高跟鞋',
-                        number: '10'
-                    },
-                    {
-                        name: '袜子',
-                        number: '20'
-                    },
-                ]
-            }
-        },
+<div id="myChart" style="width: 800px;height:400px;"></div>
+
+</template> 
+
+ <script >
+
+let echarts=require('echarts')
+export default {
+  
+
+  // data () {
+  //   return {
+      
+  //   }
+  // },
+  mounted(){
+     this.myChart=echarts.init(this.$el);
+    this.drawLine();
+   
+  },
+  methods: {
+    drawLine(){
+        // 基于准备好的dom，初始化echarts实例
         
+        // 绘制图表
+        this.myChart.setOption({
+            title: { 
+              text: 'ECharts 入门示例' 
+            },
+            tooltip: {},
+            legend: {
+                data:['销量']
+            },
+            xAxis: {
+                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        });
     }
+  }
+}
 </script>
-<style>
- 
-</style>
