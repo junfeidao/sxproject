0f9ebd5e66a776846930fe7a2ae0fc1231f7da3b
CherrySUKURA <18345516193@163.com>
2019-01-08 14:33:29 +0800

    feat(cp-wzh-Table/src/meTable.vue): 行内修改数据

--- src/components/cp-wzh-Table/src/meTable.vue
+++ src/components/cp-wzh-Table/src/meTable.vue
@@ -2,32 +2,32 @@
   <div style="float:left;margin-top:50px;width:800px;">
     <ElTable :data="tableData" style="width: 100%">
       <ElTableColumn prop="name" label="姓名" width="180" />
-      <ElTableColumn prop="value" label="数量" width="180" />
       <ElTableColumn label="改变数量" width="180">
         <template slot-scope="scope">
-          <ElInput v-if="show" v-model="tableData[scope.$index].value" />
+          <span>{{ scope.row.value }}</span>
+          <ElInput v-if="show" v-model="tableData[scope.$index].value" :display="show" />
         </template>
       </ElTableColumn>
       <ElTableColumn label="确认修改" width="180">
         <template slot-scope="scope">
           <ElButton
             v-if="!show"
-            ref="btn"
+            :display="show"
             class="button-one"
             type="primary"
             @click="showout(scope.$index)"
           >
-{{ text }}
-</ElButton>
+            点击修改
+          </ElButton>
           <ElButton
             v-else
-            ref="btn2"
             class="button-one"
+            :display="show"
             type="primary"
             @click="showdown(scope.$index)"
           >
-{{ text2 }}
-</ElButton>
+            确认修改
+          </ElButton>
         </template>
       </ElTableColumn>
     </ElTable>
@@ -46,18 +46,17 @@ export default {
   data() {
     return {
       show: false,
-      text: "点击修改",
-      text2: "确认修改"
+      index: ""
     }
   },
   methods: {
     showout(index) {
-      this.$refs.btn.show = "true"
       if (this.show === true) {
         this.show = false
       } else {
         this.show = true
       }
+      // this.index = index
     },
     showdown(index) {
       if (this.show === true) {
