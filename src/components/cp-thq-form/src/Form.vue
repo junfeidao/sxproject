8284997cdd58869f096a07d24aa26b049bb5ef92
taohaoqin <445943123@qq.com>
2019-01-15 09:53:00 +0800

    feat(components/cp-thq-form/Form.vue,cp-thq-table/MyEcharts.vue ,pages/thq/,thq.vue): 表单添加功能

---
+++ src/components/cp-thq-form/src/Form.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div class="Add" @click="toAdd">
+      +
+    </div>
+    <ElDialog width="20%" :visible.sync="visible" title="添加栏">
+      <ElForm label-width="50px">
+        <ElFormItem label="种类">
+          <ElInput v-model="name" />
+        </ElFormItem>
+        <ElFormItem label="销量">
+          <ElInput v-model="number" />
+        </ElFormItem>
+        <ElFormItem>
+          <ElButton type="primary" @click="submitForm">
+            确定
+          </ElButton>
+          <ElButton @click="resetForm">
+            取消
+          </ElButton>
+        </ElFormItem>
+      </ElForm>
+    </ElDialog>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    tableData: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      visible: false,
+      name: '',
+      number: ''
+    }
+  },
+  watch: {
+    visible() {
+      this.name = ''
+      this.number = ''
+    }
+  },
+  methods: {
+    submitForm() {
+      this.tableData.push({
+        name: this.name,
+        number: this.number
+      })
+      this.visible = false
+    },
+    resetForm() {
+      this.visible = false
+    },
+    toAdd() {
+      this.visible = true
+    }
+  }
+}
+</script>
