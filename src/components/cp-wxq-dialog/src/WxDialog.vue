9349ebb2f6bc8e82fd296d068d717cf5bc8d2297
weixingqi <1306506962@qq.com>
2019-01-21 09:14:34 +0800

    feat(WxDialog.vue): 表单验证优化 部分笔记

--- src/components/cp-wxq-dialog/src/WxDialog.vue
+++ src/components/cp-wxq-dialog/src/WxDialog.vue
@@ -11,6 +11,7 @@
       :visible.sync="show"
       width="30%"
       :before-close="handleClose"
+      @close="resetForm('ruleForm')"
     >
       <ElForm
         ref="ruleForm"
@@ -23,16 +24,18 @@
           <ElInput
             v-model.number="ruleForm.kind"
             size="mini"
+            autocomplete="off"
           />
         </ElFormItem>
         <ElFormItem label="销量" prop="saleCount">
           <ElInput
             v-model.number="ruleForm.saleCount"
             size="mini"
+            autocomplete="off"
           />
         </ElFormItem>
         <ElFormItem>
-          <ElButton @click="resetForm('ruleForm')">
+          <ElButton @click="show = false">
             取 消
           </ElButton>
           <ElButton type="primary" @click="submitForm('ruleForm')">
@@ -87,14 +90,11 @@ export default {
     },
     resetForm(formName) {
       this.$refs[formName].resetFields()
-      this.show = false
     },
     handleClose(done) {
       this.$confirm('确认关闭？')
         .then(_ => {
           done()
-          this.ruleForm.kind = ''
-          this.ruleForm.saleCount = ''
         })
         .catch(_ => {})
     }
